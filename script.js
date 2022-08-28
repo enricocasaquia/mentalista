cf64e271abdba983f42c107b6c36c3ce2e8add12
Enrico Casaquia Fernandes <101847703+enricocasaquia@users.noreply.github.com>
2022-08-28 11:31:19 -0300

    melhor elaboração do script

--- script.js
+++ script.js
@@ -2,29 +2,131 @@
 var númeroSecreto = parseInt(Math.random()*11);
 
 //Função que confirma o valor inserido e dá uma resposta ao usuário
-function Chutar() {
+function Chutar1() {
 
     //Variável que busca o valor inserido pelo usuário
     var chute = parseFloat(document.getElementById("valor").value); 
 
-    //Variável utilizada para definir um conteúdo a ser exibido com "innerHTML"
+    //Variável utilizada para definir um conteúdo a ser exibido posteriormente com "innerHTML"
     var respostaExibida = document.getElementById("resultado");
+    var respostaExibida2 = document.getElementById("resultado2");
   
     //Condicional caso o valor inserido for equivalente ao número sorteado
     if(chute == númeroSecreto) {
+        
+        //Habilita a exibição da caixa de texto na tela
+        document.getElementById("resultado").hidden = false;
+        
         respostaExibida.innerHTML = "Você acertou!";
+
+        //Desabilita  e habilita os botões na tela
+        document.getElementById("botao").disabled = true;
+        document.getElementById("btnJogarNovamente").disabled = false;
     }
     //Condicional coso o valor inserido não respeite as exigências (estar entre 0 e 10), o que indica nesse caso não desejado, que a variável "chute" seja maior que 10 ou menor que 0 
     else if(chute>10 || chute<0) {
-        respostaExibida.innerHTML = "Insira um valor entre 0 e 10";
+        //Exibe um alerta na tela
+        alert("Insira um valor entre 0 e 10");
     }
     //Condicional caso o valor inserido seja um número decimal
     else if(chute % 1 != 0) {
-        respostaExibida.innerHTML = "Insira um número inteiro";
+        alert("Insira um número inteiro");
     }
     //Condicional caso nenhuma das condicionais acima sejam atendidas
     else {
-        respostaExibida.innerHTML = "Você errou, o número secreto era " + númeroSecreto;
+        if(númeroSecreto <= 5) {
+            document.getElementById("resultado").hidden = false;
+            document.getElementById("resultado2").hidden = false;
+
+            //Exibe a mensagem na tela
+            respostaExibida.innerHTML = "<span style='color:#00FF00'>Você errou mas ainda tem mais 2 chances</span>";
+            respostaExibida2.innerHTML = "<span style='color:#00FF00'>O número é menor ou igual a 5</span>";
+        }
+        else {
+            document.getElementById("resultado").hidden = false;
+            document.getElementById("resultado2").hidden = false;
+            
+            respostaExibida.innerHTML = "<span style='color:#00FF00'>Você errou mas ainda tem mais 2 chances</span>";
+            respostaExibida2.innerHTML = "<span style='color:#00FF00'>O número é maior que 5</span>";
+        }
+        //Chama a próxima função ao apertar o botão novamente
+        document.getElementById("botao").onclick = Chutar2;
+    }
+} 
+
+function Chutar2() {
+    
+    var chute = parseFloat(document.getElementById("valor").value);
+
+    var respostaExibida = document.getElementById("resultado");
+    var respostaExibida2 = document.getElementById("resultado2");
+
+    if(chute == númeroSecreto) {
+        document.getElementById("resultado2").hidden = true;
+
+        respostaExibida.innerHTML = "Você acertou!";
+
+        document.getElementById("botao").disabled = true;
+        document.getElementById("btnJogarNovamente").disabled = false;
+    }
+     
+    else if(chute>10 || chute<0) {
+        alert("Insira um valor entre 0 e 10");
+    }
+    
+    else if(chute % 1 != 0) {
+        alert("Insira um número inteiro");
     }
 
-} 
+    else {
+        if(númeroSecreto <= 5) {
+            document.getElementById("resultado").hidden = false;
+            document.getElementById("resultado2").hidden = false;
+
+            respostaExibida.innerHTML = "<span style='color:#FF8C00'>Você errou mas ainda tem mais 1 chance</span>";
+            respostaExibida2.innerHTML = "<span style='color:#FF8C00'>O número é menor ou igual a 5</span>";
+        }
+        else {
+            document.getElementById("resultado").hidden = false;
+            document.getElementById("resultado2").hidden = false;
+
+            respostaExibida.innerHTML = "<span style='color:#FF8C00'>Você errou mas ainda tem mais 1 chance</span>";
+            respostaExibida2.innerHTML = "<span style='color:#FF8C00'>O número é maior que 5</span>";
+        }
+        document.getElementById("botao").onclick = Chutar3;
+    }
+}
+
+function Chutar3() {
+    
+    var chute = parseFloat(document.getElementById("valor").value);
+
+    var respostaExibida = document.getElementById("resultado");
+    var respostaExibida2 = document.getElementById("resultado2");
+
+    if(chute == númeroSecreto) {
+        document.getElementById("resultado2").hidden = true;
+
+        respostaExibida.innerHTML = "Você acertou!";
+
+        document.getElementById("botao").disabled = true;
+        document.getElementById("btnJogarNovamente").disabled = false;
+    }
+     
+    else if(chute>10 || chute<0) {
+        alert("Insira um valor entre 0 e 10");
+    }
+    
+    else if(chute % 1 != 0) {
+        alert("Insira um número inteiro");
+    }
+
+    else {
+        document.getElementById("resultado2").hidden = true;
+
+        respostaExibida.innerHTML = "<span style='color:#FF0000'>Você errou!! O número secreto era: </span>" + númeroSecreto;
+
+        document.getElementById("botao").disabled = true;
+        document.getElementById("btnJogarNovamente").disabled = false;
+    }
+}
